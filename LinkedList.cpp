d72d9d0a885ea0f7240f16105735097be7047201
jin1408 <76673520+jin1408@users.noreply.github.com>
2021-01-16 11:40:20 +0900

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,6 +5,8 @@ using namespace std;
 //c++을 사용한 단순 연결 리스트 구현
 // +연결 리스트의 모든 데이터 값을 출력하는 프로그램(Sum함수)
 // +연결 리스트의 특정 데이터 값의 개수를 찾는 프로그램(Search함수)
+// +연결 리스트의 모든 노드의 순서를 뒤집는 프로그램(Reverse함수)
+// +연결 리스트와 다른 연결 리스트를 병합하는 프로그램(Merge함수)
 
 class Node{
     Node* link;
@@ -113,6 +115,20 @@ public:
         }
         cout<<"리스트 내의 'k' 개수 : "<<count<<endl;
     }
+    void Reverse(){
+        int count=0;
+        for(int i=0; i<size(); i++){
+            Node *p = remove(size()-1);
+            insert(count++,p);
+        }
+    }
+
+    void Merge(LinkedList *that){
+        while(!(that->isEmpty())){
+            Node *p = that->remove(0);
+            insert(size(),p);
+        }
+    }
 
 };
 
@@ -121,12 +137,18 @@ public:
 int main()
 {
     LinkedList list;
+    LinkedList list2;
     list.insert(0, new Node(10));
     list.insert(0, new Node(20));
     list.insert(1, new Node(30));
-    list.insert(2, new Node(20));
-    list.Sum();
-    list.Search(20);
+    list.insert(2, new Node(50));
+    list2.insert(0, new Node(100));
+    list2.insert(0, new Node(101));
+    list2.insert(1, new Node(212));
+    list2.insert(2, new Node(413));
+    list.Reverse();
+    list.display();
+    list.Merge(&list2);
     list.display();
 
 	return 0;
