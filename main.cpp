f43e2e01a4a59795808ebd51b994058f88df94c3
jin1408 <76673520+jin1408@users.noreply.github.com>
2021-01-20 00:16:51 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+int Left = 0;;
+int Right = 40;
+int shape[40];
+
+void display(int* s,int left, int right){
+    for(int i=left; i<right; i++){
+        if(s[i])
+            cout<<"X";
+        else
+            cout<<"-";
+    }
+    cout<<endl;
+}
+
+void draw_tree(int row, int left, int right){
+    int center = right/2;
+    for(int i=Left; i<Right; i++)
+        shape[i]=0;
+
+    if(row==0){
+        return ;
+    }
+    else{
+        for(int i=0; i<4-row; i++){
+            shape[center] = 1;
+            shape[center+Right/2] = 1;
+            center = (center*2)+center;
+        }
+        display(shape, Left, Right);
+        draw_tree(row-1,left,right/2);
+
+    }
+}
+
+int main()
+{
+    int row;
+
+    cout<<"How many rows?"<<endl;
+    cin>>row;
+    draw_tree(row, Left, Right);
+	return 0;
+}
