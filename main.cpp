0b7e042aaef7951c3e3c2df87e0a3eb32e32a400
jin1408 <76673520+jin1408@users.noreply.github.com>
2021-01-19 15:57:39 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+int fib(int *count, int n){
+    count[n]++;
+    if(n<2){return n; }
+    else{
+        return (fib(count, n-1)+fib(count, n-2));
+    }
+
+}
+
+int main()
+{
+    int num;
+    cin>> num;
+    int *count = new int[num+1];
+    for(int i=0; i<num+1; i++)
+        count[i] =0;
+    fib(count, num);
+    for(int i=0; i<num+1; i++)
+        printf("Fibo(%d) = %d¹ø\n",i,count[i]);
+
+	return 0;
+}
